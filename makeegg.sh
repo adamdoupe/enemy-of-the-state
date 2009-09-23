e821c0375344eff27c0a00d985cc3a09b4e63d97
Ludovico Cavedon <cavedon@cs.ucsb.edu>
2009-09-22 17:06:53 -0700

    support htmlunit 2.6

--- makeegg.sh
+++ makeegg.sh
@@ -1,8 +1,10 @@
-javac -classpath htmlunit-2.5/lib/htmlunit-2.5.jar *.java
+VER=2.6
+
+javac -classpath htmlunit-$VER/lib/htmlunit-$VER.jar *.java
 
 
 RESERVED="--reserved READONLY --reserved T_SHORT --reserved T_INT --reserved T_LONG --reserved T_FLOAT --reserved T_DOUBLE --reserved T_STRING --reserved T_OBJECT --reserved T_CHAR --reserved T_BYTE --reserved T_UBYTE --reserved T_USHORT --reserved T_UINT --reserved T_ULONG --reserved T_STRING_INPLACE --reserved T_BOOL --reserved T_OBJECT_EX --reserved T_LONGLONG --reserved T_ULONGLONG --reserved T_PYSSIZET --reserved READONLY --reserved RO --reserved READ_RESTRICTED --reserved PY_WRITE_RESTRICTED --reserved RESTRICTED"
 
-PARAMS="--python htmlunit --jar htmlunit-2.5/lib/htmlunit-2.5.jar --jar htmlunit-2.5/lib/htmlunit-core-js-2.5.jar $(for f in htmlunit-2.5/lib/[cnsx]*.jar; do echo ' --include' $f; done) --exclude com.gargoylesoftware.htmlunit.html.HtmlTableCell --exclude net.sourceforge.htmlunit.corejs.javascript.ScriptRuntime --version 2.5.0 --package java.io --package java.net --classpath . HtmlPageWrapper HtmlElementWrapper --package java.lang java.lang.System --mapping java.util.Properties 'getProperty:(Ljava/lang/String;)Ljava/lang/String;'"
+PARAMS="--python htmlunit --jar htmlunit-$VER/lib/htmlunit-$VER.jar --jar htmlunit-$VER/lib/htmlunit-core-js-$VER.jar $(for f in htmlunit-$VER/lib/[cnsx]*.jar; do echo ' --include' $f; done) --exclude com.gargoylesoftware.htmlunit.html.HtmlTableCell --exclude net.sourceforge.htmlunit.corejs.javascript.ScriptRuntime --exclude net.sourceforge.htmlunit.corejs.javascript.Node --version $VER.0 --package java.io --package java.net --classpath . HtmlPageWrapper HtmlElementWrapper --package java.lang java.lang.System --mapping java.util.Properties 'getProperty:(Ljava/lang/String;)Ljava/lang/String;' --files separate"
 
 python -m jcc.__main__ $PARAMS $RESERVED --build --files 1 && python -m jcc.__main__ $PARAMS $RESERVED --bdist
