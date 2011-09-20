d7d5a32fe49eef768fab874b771c94e7c2d8c362
Adam Doupe <adamdoupe@gmail.com>
2011-09-20 12:04:34 -0700

    Fixed bug with forms that have no named parameters.

--- crawler2.py
+++ crawler2.py
@@ -2797,6 +2797,8 @@ class Crawler(object):
         #self.webclient = htmlunit.WebClient(bw)
         self.webclient = htmlunit.WebClient()
         self.webclient.setThrowExceptionOnScriptError(True);
+
+        self.webclient.setThrowExceptionOnFailingStatusCode(True);
         self.webclient.setUseInsecureSSL(True)
         self.webclient.setRedirectEnabled(False)
         self.refresh_urls = []
@@ -3373,7 +3375,7 @@ class FormFiller(object):
             return FormFiller.Params()
         values = self.get(keys, form)
         if not values:
-            return None
+            return FormFiller.Params()
         return values.getnext()
 
 
---
+++ test/sites/form_empty_params/purchase.php
@@ -0,0 +1,21 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] != 'POST')
+{
+   exit(0);
+}
+
+?>
+
+<html><head><title>Form with no-named params test</title></head>
+
+<body>
+
+<div><h1>GREAT, you just purchased something!</h1>
+<p>
+    Wow, look at this cool new thing you've purchased. Isn't it awesome?
+</p>
+
+</body>
+
+</html>
